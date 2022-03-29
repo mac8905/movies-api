ded1f22606aefc216f1b5906772dfe6b087527c1
Miguel Caro <miguel.caro@sideinc.com>
2022-03-29 09:45:04 -0500

    feat(crud): added crud for entities

--- src/resolvers/mutation.ts
+++ src/resolvers/mutation.ts
@@ -8,10 +8,43 @@ export const Mutation = {
   ) => {
     return context.orm.user.create({
       data: {
-        name: args.input.name,
-        email: args.input.email,
-        password: args.input.password,
+        ...args.input,
       },
+    });
+  },
+
+  createMovie: (
+    _parent: unknown,
+    args: { input: Prisma.MovieCreateInput },
+    context: { orm: PrismaClient },
+  ) => {
+    return context.orm.movie.create({
+      data: {
+        ...args.input,
+      },
+    });
+  },
+
+  updateMovie: (
+    _parent: unknown,
+    args: { id: string; input: Prisma.MovieUpdateInput },
+    context: { orm: PrismaClient },
+  ) => {
+    return context.orm.movie.update({
+      where: { id: args.id },
+      data: {
+        ...args.input,
+      },
+    });
+  },
+
+  deleteMovie: (
+    _parent: unknown,
+    args: { id: string },
+    context: { orm: PrismaClient },
+  ) => {
+    return context.orm.movie.delete({
+      where: { id: args.id },
     });
   },
 };
--- src/resolvers/query.ts
+++ src/resolvers/query.ts
@@ -1,1 +1,21 @@
-export const Query = {};
+import type { PrismaClient } from '@prisma/client';
+
+export const Query = {
+  movies: (
+    _parent: unknown,
+    _args: unknown,
+    context: { orm: PrismaClient },
+  ) => {
+    return context.orm.movie.findMany();
+  },
+
+  movie: (
+    _parent: unknown,
+    args: { id: string },
+    context: { orm: PrismaClient },
+  ) => {
+    return context.orm.movie.findFirst({
+      where: { id: args.id },
+    });
+  },
+};
