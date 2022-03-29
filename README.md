7971d40f166b05ec42b150752c963e318708ad6c
Miguel Caro <miguel.caro@sideinc.com>
2022-03-28 23:17:32 -0500

    feat(users): added migration strategy and first mutation for creating users

--- README.md
+++ README.md
@@ -9,4 +9,6 @@ HTTP server api for performing CRUD operations on movies.
 ## How to start the api
 
 1. Install dependencies `yarn`
-2. `yarn dev`
+2. Run docker db container `docker-compose up -d`
+3. Run migration `yarn migrate`
+4. `yarn dev`
--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,12 @@
 import { ApolloServer } from 'apollo-server';
-import path from 'path';
 import { readFileSync } from 'fs';
+import { PrismaClient } from '@prisma/client';
+import path from 'path';
+import resolvers from './resolvers';
 
+const orm = new PrismaClient();
 const typeDefs = readFileSync(path.join(__dirname, 'schema.graphql'), 'utf-8');
-
-const resolvers = {
-  Query: {},
-  Mutation: {
-    register: (
-      _parent: any,
-      args: { input: any },
-      _context: { dataSources: { userAPI: { register: (arg0: any) => any } } },
-      _info: any,
-    ) => {
-      return {
-        id: '1',
-        name: args.input.name,
-        email: args.input.email,
-        password: args.input.password,
-      };
-    },
-  },
-};
-
-const server = new ApolloServer({ typeDefs, resolvers });
+const server = new ApolloServer({ typeDefs, resolvers, context: { orm } });
 
 server.listen().then(({ url }) => {
   console.log(`🚀 Server ready at ${url}`);
